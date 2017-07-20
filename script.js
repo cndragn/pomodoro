88a3a8afa30d1fd2313ea7ed427b2d38447e66ab
CNDavidson <cndragn@gmail.com>
2017-07-20 09:37:27 -0400

    Add functions to convert time to/from milliseconds

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     var sessionTime = "25";
     var breakTime = "5";
     var oper = ""; //operator to increment by plus or minus
+    var displayTime = "00m 00s";
 
     //get data when timer buttons are clicked
     document.getElementById("setTime").addEventListener("click", getIncrement);
@@ -12,14 +13,25 @@ $(document).ready(function() {
         var lessMore = t.target.id;
         compute(lessMore);
     }
+        function convertFrom(milliseconds) {
+    	var totalSeconds = Math.floor(milliseconds/1000);
+    	var minutes = Math.floor(totalSeconds/60);
+    	var seconds = totalSeconds - minutes * 60;
+    }
+
+        function convertTo(milliseconds) {
+    	var seconds = milliseconds * 60;
+    	var milliseconds = milliseconds * 1000;
+
+    	console.log("ms: " + milliseconds);
+    }
 
     //display times
     function displayIt(display, func) {
         if (func == "minus") {
             if (display == "sessionLess") {
-                console.log(sessionTime);
                 sessionTime -= 1;
-                console.log(sessionTime);
+                convertTo(sessionTime);
             }
             if (display == "breakLess") {
                 breakTime -= 1;
@@ -38,6 +50,8 @@ $(document).ready(function() {
 
         document.getElementById("work").innerHTML = sessionTime + "m";
         document.getElementById("rest").innerHTML = breakTime + "m";
+
+        document.getElementById("displayTime").innerHTML = sessionTime + "m" + "00s";
     }
 
     //Button functions
@@ -54,16 +68,27 @@ $(document).ready(function() {
      //Timer Bar
     var elem = document.getElementById("timer-bar");
     var width = 1;
-    var id = setInterval(frame, 3000); //1 second = 1000 milliseconds
+    var intTime = 1000;
+    var id = setInterval(frame, intTime); //1 second = 1000 milliseconds
+    var intTimeCalc = intTime;
+
+
+
+
 
     //Timer movement
     function frame() {
+    	
+
         if (width >= 100) {
             clearInterval(id);
         } else {
             width++;
+            
             elem.style.width = width + '%';
         }
+        intTimeCalc -= 10;
+
     }
 
 
